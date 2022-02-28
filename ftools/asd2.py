Added window and average bin function With these two functions, the foundation of ASD is complete but more bug testing and checking is needed

ftools/asd2.py
@@ -1,6 +1,7 @@
 """
 file: asd2.py
 """
+from .asd import asd
 import numpy as np
 
 # Define windows dictionary
@@ -99,9 +100,46 @@ def asd2(time_series, sample_rate, smooth_width=9, detrend=1,
     time_series = apply_detrend(time_series, order=detrend)
 
     # 2) Window the time series
-    # time_series = apply_window(time_series, window_name=window)
+    time_series = apply_window(time_series, window_name=window)
 
-    return time_series
+    # 3) Take ASD
+    freq, mag = asd(time_series, sample_rate)
+
+    # 4) Average over adjacent bins
+    freq, mag = average_bins(freq, mag, smooth_width)
+
+    # Return frequency and magnitude of ASD
+    return freq, mag
+
+
+def average_bins(freq, mag, smooth_width):
+    """ This function averages over adjacent frequency bins
+
+    The ASD is broke into bins of width 'smooth_width'
+    and then averaged together
+    """
+
+    # Get number of current points and new points
+    N = len(freq)
+    new_N = int(np.floor(N/smooth_width))
+
+    # Initialize new frequency domain and new mag
+    new_freq = np.zeros(new_N)
+    new_mag = np.zeros(new_N)
+
+    # Populate new frequencies and magnitudes
+    for i in range(new_N):
+
+        # Get freq/mag bin
+        freq_bin = freq[i*smooth_width:(i+1)*smooth_width]
+        mag_bin = mag[i*smooth_width:(i+1)*smooth_width]
+
+        # Set new freq to linear average and set new mag to RMS average
+        new_freq[i] = np.mean(freq_bin)
+        new_mag[i] = np.sqrt(np.mean(mag_bin**2))
+
+    # Return new frequency and magnitude
+    return new_freq, new_mag
 
 
 def apply_window(time_series, window_name='hanning'):

tests/test_asd2.py
@@ -1,13 +1,13 @@
 """
 file: test_asd.py
 
-This file tests the asd function of ftools
+This file tests the asd2 function of ftools
 """
 
-from ftools import asd
+from ftools import asd2
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import integrate
+from scipy import integrate, signal
 
 AMPLITUDE_1 = 2
 AMPLITUDE_2 = 1
@@ -15,9 +15,9 @@ AMPLITUDE_2 = 1
 
 def main():
     # Get time series
-    sample_rate = 512
+    sample_rate = 2048
     start_time = 0
-    end_time = 600
+    end_time = 300
     time_series = sample(start_time, end_time, sample_rate)
 
     # Get local times
@@ -25,22 +25,36 @@ def main():
     t = np.linspace(0, N/sample_rate, num=N)
 
     # Get asd
-    freq, mag = asd(time_series, sample_rate)
+    freq, mag = asd2(time_series, sample_rate, smooth_width=9, detrend=5)
+
+    # Get PSD with welch
+    N_dft = int(sample_rate / 0.025)
+    freq1, psd1 = signal.welch(
+        time_series.astype(np.float64),
+        fs=sample_rate,
+        window='hann',
+        nperseg=N_dft,
+        noverlap=N_dft/2,
+        detrend='constant',
+        return_onesided=True,
+        scaling='density',
+    )
+    mag1 = np.sqrt(psd1)
 
     # Check RMS
-    print(f"The max peak is {max(mag)}")
     check_RMS(freq, mag)
+    check_RMS(freq1, mag1)
 
     # Plot ASD and Time Series
-    plot(t, time_series, freq, mag)
+    plot(t, time_series, freq, mag, freq1, mag1)
 
 
-def plot(t, time_series, freq, mag):
+def plot(t, time_series, freq, mag, freq1, mag1):
     # Set up subplots
     figure, axis = plt.subplots(2, 1)
     figure.set_figheight(10)
     figure.set_figwidth(8)
-    figure.suptitle("Demonstration of asd function")
+    figure.suptitle("Demonstration of asd2 function")
 
     # Plot original function
     axis[0].set_title("Time Series")
@@ -55,6 +69,7 @@ def plot(t, time_series, freq, mag):
     axis[1].set_xlabel("Frequency [Hz]")
     axis[1].semilogx()
     axis[1].plot(freq, np.abs(mag), label="ASD")
+    axis[1].plot(freq1, np.abs(mag1), label="ASD from Welch")
     axis[1].legend()
 
     plt.savefig("image.png")
@@ -81,10 +96,10 @@ def sample(start, end, sample_rate):
 
 def signal_generator(t):
     # Set frequency and get period of sin function
-    freq_1 = 1
+    freq_1 = 2
     period_1 = freq_1*2*np.pi
 
-    freq_2 = 5
+    freq_2 = 0.1
     period_2 = freq_2*2*np.pi
 
     # Lets just say units of m

tests/test_window.py
@@ -1,10 +1,9 @@
 """
-file: test_detrend.py
+file: test_window.py
 
-This file tests the apply_detrend function of ftools
+This file tests the apply_window function of ftools
 """
-
-from ftools.asd2 import apply_detrend
+from ftools.asd2 import apply_window
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -23,38 +22,32 @@ def main():
     t = np.linspace(0, N/sample_rate, num=N)
 
     # De-trend time series
-    detrended = apply_detrend(raw_time_series, order=2)
-
-    # Get original wave and trend
-    orginal_wave = sin_wave(t)
-    trend = get_trend(t)
+    windowed = apply_window(raw_time_series, window_name="hanning")
 
     # Plot
-    plot(t, raw_time_series, trend, orginal_wave, detrended)
+    plot(t, raw_time_series, windowed)
 
 
-def plot(t, raw_time_series, trend, orginal_wave, detrended):
+def plot(t, raw_time_series, windowed):
 
     # Set up subplots
     figure, axis = plt.subplots(2, 1)
     figure.set_figheight(10)
     figure.set_figwidth(8)
-    figure.suptitle("Demonstration of apply_detrend function")
+    figure.suptitle("Demonstration of apply_window function")
 
     # Plot original function
-    axis[0].set_title("Non-detrended Data")
+    axis[0].set_title("Non-windowed Data")
     axis[0].set_ylabel("Motion [m]")
     axis[0].set_xlabel("Time [s]")
     axis[0].plot(t, raw_time_series, label="Raw Time Series")
-    axis[0].plot(t, trend, label="Applied Trend")
     axis[0].legend()
 
     # Plot de-trended function
-    axis[1].set_title("Detrended Data")
+    axis[1].set_title("Windowed Data")
     axis[1].set_ylabel("Motion [m]")
     axis[1].set_xlabel("Time [s]")
-    axis[1].plot(t, detrended, label="Detrended function")
-    axis[1].plot(t, orginal_wave, label="Original Sin Wave")
+    axis[1].plot(t, windowed, label="Detrended function")
     axis[1].legend()
 
     plt.savefig("image.png")
@@ -70,8 +63,7 @@ def sample(start, end, sample_rate):
 
 
 def signal_generator(t):
-    # Combine trend and sin wave
-    return sin_wave(t) + get_trend(t)
+    return sin_wave(t)
 
 
 def sin_wave(t):
@@ -81,10 +73,6 @@ def sin_wave(t):
 
     # Return sin wave
     return amplitude*np.sin(2*np.pi*freq*t)
-
-
-def get_trend(t):
-    return 0.005*t**2
 
 
 if __name__ == "__main__":