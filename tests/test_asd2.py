da38c768c06584601ee57c36f03f2e1f97bda134
Sam Scherf <csmoran@protonmail.com>
2022-03-09 23:26:02 -0800

    Updated ASD2 test to better demonstrate features

--- tests/test_asd2.py
+++ tests/test_asd2.py
@@ -7,17 +7,18 @@ This file tests the asd2 function of ftools
 from ftools import asd2
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import integrate, signal
+from scipy import integrate
 
 AMPLITUDE_1 = 2
 AMPLITUDE_2 = 1
+AMPLITUDE_3 = 0.5
 
 
 def main():
     # Get time series
     sample_rate = 2048
     start_time = 0
-    end_time = 300
+    end_time = 95
     time_series = sample(start_time, end_time, sample_rate)
 
     # Get local times
@@ -25,31 +26,16 @@ def main():
     t = np.linspace(0, N/sample_rate, num=N)
 
     # Get asd
-    freq, mag = asd2(time_series, sample_rate, smooth_width=9, detrend=5)
-
-    # Get PSD with welch
-    N_dft = int(sample_rate / 0.025)
-    freq1, psd1 = signal.welch(
-        time_series.astype(np.float64),
-        fs=sample_rate,
-        window='hann',
-        nperseg=N_dft,
-        noverlap=N_dft/2,
-        detrend='constant',
-        return_onesided=True,
-        scaling='density',
-    )
-    mag1 = np.sqrt(psd1)
+    freq, mag = asd2(time_series, sample_rate, smooth_width=9, detrend=0)
 
     # Check RMS
     check_RMS(freq, mag)
-    check_RMS(freq1, mag1)
 
     # Plot ASD and Time Series
-    plot(t, time_series, freq, mag, freq1, mag1)
+    plot(t, time_series, freq, mag)
 
 
-def plot(t, time_series, freq, mag, freq1, mag1):
+def plot(t, time_series, freq, mag):
     # Set up subplots
     figure, axis = plt.subplots(2, 1)
     figure.set_figheight(10)
@@ -60,17 +46,14 @@ def plot(t, time_series, freq, mag, freq1, mag1):
     axis[0].set_title("Time Series")
     axis[0].set_ylabel("Motion [m]")
     axis[0].set_xlabel("Time [s]")
-    axis[0].plot(t, time_series, label="Raw Time Series")
-    axis[0].legend()
+    axis[0].plot(t, time_series)
 
     # Plot ASD
     axis[1].set_title("Amplitude Spectral Density")
     axis[1].set_ylabel("Displacement [m/sqrt(Hz)]")
     axis[1].set_xlabel("Frequency [Hz]")
     axis[1].semilogx()
-    axis[1].plot(freq, np.abs(mag), label="ASD")
-    axis[1].plot(freq1, np.abs(mag1), label="ASD from Welch")
-    axis[1].legend()
+    axis[1].plot(freq, np.abs(mag))
 
     plt.savefig("image.png")
 
@@ -78,7 +61,7 @@ def plot(t, time_series, freq, mag, freq1, mag1):
 def check_RMS(freq, mag):
     # Get observed and expected RMS
     observed_RMS = np.sqrt(integrate.trapezoid(mag**2, x=freq))
-    expected_RMS = np.sqrt((0.707*AMPLITUDE_1)**2 + (0.707*AMPLITUDE_2)**2)
+    expected_RMS = 0.707*np.sqrt(AMPLITUDE_1**2+AMPLITUDE_2**2+AMPLITUDE_3**2)
 
     # Display observed and expected RMS
     print(f"The expected RMS is {expected_RMS}")
@@ -102,8 +85,20 @@ def signal_generator(t):
     freq_2 = 0.1
     period_2 = freq_2*2*np.pi
 
+    freq_3 = 50
+    period_3 = freq_3*2*np.pi
+
     # Lets just say units of m
-    return AMPLITUDE_1*np.sin(period_1*t)+AMPLITUDE_2*np.sin(period_2*t)
+    signal = AMPLITUDE_1*np.sin(period_1*t)
+    signal += AMPLITUDE_2*np.sin(period_2*t)
+    signal += AMPLITUDE_3*np.sin(period_3*t)
+    signal += trend(t)
+
+    return signal
+
+
+def trend(t):
+    return 0.00005*(t-100)**2
 
 
 if __name__ == "__main__":
