6c577712b9c29dd1b5295c20b097ce8f45375f50
Sam <sam@github.com>
2022-03-10 10:23:07 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ftools',
+      version='1',
+      description='Tools for analyzing signals',
+      author='Sam Scherf',
+      author_email='github@samscherf.com',
+      license='GPL-3.0',
+      packages=['ftools'],
+      install_requires=['scipy', 'numpy'])
